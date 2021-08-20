339c209954f05e69d8303792f9a5574530d1c306
Vladislav <31815075+vladkulikov@users.noreply.github.com>
2021-08-20 21:11:33 +0300

    Delete src/main/java/com/example/demo/repositories directory

--- src/main/java/com/example/demo/repositories/EntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.Entity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EntityRepository {
-    Entity getEntityById(long id);
-}
